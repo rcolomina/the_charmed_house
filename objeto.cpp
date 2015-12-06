afa116fa10309077c5f96a9dc0e8affa5c7648c8
rcololomina <rcolomina@gmail.com>
2015-12-06 04:27:55 -0500

    New list of attributes with getters and setters. New methods pull (tirar).

--- objeto.cpp
+++ objeto.cpp
@@ -1,14 +1,34 @@
 #include "objeto.h"
 
-Objeto::Objeto(const int id,
-					const string nombre,
-					const string descripcion):
-	  id(id),nombre(nombre),descripcion(descripcion) 
+
+Objeto::Objeto(int id,
+					string nombre,
+					string descripcion):Entidad(id,nombre,descripcion)
 {
 	  usado=false;
 	  fijo=false;
 	  visible=true;
+	  tirable=false;
+	  alcanzable=true;
 }
+
+void Objeto::tirar()
+{
+	  cout<<endl<<descripcion_tirar<<endl;
+	  if(objeto_rel_tirar!=0)
+			 objeto_rel_tirar->set_visible(true);
+	  else
+			 cout<<"ERROR: Null object"<<endl;			 
+}
+
+void Objeto::set_tirable(Objeto *objeto,string descrip_tirable)
+{
+	  tirable=true;
+	  this->objeto_rel_tirar=objeto;
+	  this->descripcion_tirar=descrip_tirable;
+}
+
+
 
 
 
--- objeto.h
+++ objeto.h
@@ -2,23 +2,17 @@
 #define _objeto_
 
 #include <string>
+#include <iostream>
+
+#include "entidad.h"
 
 using namespace std;
 
-
-//#include "escenario.h"
-
-class Item{
+class Objeto : public Entidad{
 	public:
-	  Item(){}
-	  ~Item(){}
+	  Objeto(){}
+	  Objeto(int id,string nombre,string descripcion);
 
-};
-
-
-class Objeto : public Item{
-	public:
-	  Objeto(const int id,const string nombre,const string descripcion);
 	  void usar_sobre(Objeto *objeto);
 
 	  //void eliminar(Objeto objeto); //si no está no hay nada que quitar
@@ -26,29 +20,61 @@ class Objeto : public Item{
 	  //void dejar_objeto();
 
 	  //GETTERS
-	  int get_id(){return id;}
-	  string get_nombre(){return nombre;}
-	  string get_descripcion(){return descripcion;}
+
 	  bool get_usado(){return usado;}
 	  bool get_visible(){return visible;}
 	  bool get_fijo(){return fijo;}
+	  bool get_tirable(){return tirable;}
+	  bool get_atascado(){return atascado;}
+	  bool get_desatascador(){return desatascador;}
+	  bool get_alcanzable(){return alcanzable;}
+	  bool get_alcanzador(){return alcanzador;}
 
 	  //SETTERS
 	  void set_fijo(bool estado){fijo=estado;}
 	  void set_visible(bool estado){visible=estado;}
 	  void set_usado(bool estado){usado=estado;}
+	  void set_tirable(Objeto *objeto,string descrip_tirable);
+	  void set_atascado(bool estado){ atascado=estado;}
+	  void set_desatascador(bool estado){ desatascador=estado;}
+	  void set_alcanzable(bool estado){ alcanzable=estado;}
+	  void set_alcanzador(bool estado){ alcanzador=estado;}
+
+	  // Operaciones
+	  void tirar();
+	  void alcanzar();
+	  void insertar();
 
 	private:
-	  const int id;
-	  const string nombre;
 	  //const string nombre_alternativo;
-	  const string descripcion;
 	  string descripcion_secundaria;
+	  string descripcion_tirar;
 
-	  //atributos
-	  bool fijo;  //es posible cogerlo para el inventario
-	  bool usado; //ya no tiene utilidad en el juego
-	  bool visible;
+	  // Hacer visible y/o alcanzable tras la accion
+	  Objeto *objeto_rel_tirar;
+	  Objeto *objeto_rel_alcanzar;
+
+	  // Otra salida tras la accion
+//	  Escenario *escena_rel_abrir;
+//	  Escenario *escena_rel_insertar;
+
+	  
+
+	  // Atributos del objeto
+	  bool fijo;         // True: No es posible cogerlo para el inventario.
+	  bool usado;        // True: Ya no tiene utilidad en el juego.
+	  bool visible;      // True: No es visible a simple vista.
+	  bool visualizador; // True: Permite examinar en detalle.
+	  bool tirable;      // True: Se puede tirar para desbloquearlo.
+	  bool atascado;     // True: Necesita ser desatascado.
+	  bool desatascador; // True: Permite desatascar otros objetos.
+	  bool alcanzable;   // True: Se puede alcanzar facil.
+	  bool alcanzador;   // True: Permite coger objeto no alcanzables
+	  bool movible;      // True: Puede cambiarse de posicion
+	  bool movedor;      // True: Sirve para mover otros objetos
+	  bool abrible;      // True: Permite ser abierto  (id 
+	  bool abridor;      // True: Sirve para abrir.
+	  bool insertable;   // True: Permite ser insertado
 
 	  //list<Objeto*> subscriptores; //los subcriptores estan pendientes de cambios del estado del objeto subscrito
 };
