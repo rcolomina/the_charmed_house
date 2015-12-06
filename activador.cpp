ead1d762a2c6e3085526796ad9ebb4cc57f55251
rcololomina <rcolomina@gmail.com>
2015-12-06 06:48:03 -0500

    New class to manage conditions, triggers and cause-efect issues.

---
+++ activador.cpp
@@ -0,0 +1,1 @@
+#include "activador.h"
---
+++ activador.h
@@ -0,0 +1,25 @@
+#include <list>
+
+#ifndef _DISPARADOR_H_
+#define _DISPARADOR_H_
+
+/*
+template <typename T>
+class Condicion {
+	public:
+	  Condicion(
+
+
+};*/
+
+// Relaciona estados entre entidades. Las causas produce efectos entre la entidades del juego. Ciertos estados de la lista de entidades causadora cambiaran los estados de la lista de las entidades en efectos.
+class Disparador{
+	public:
+	  Disparador(){}
+	private:
+	  list<Entidad*> causas;
+	  list<Entidad*> efectos;
+};
+
+
+#endif
