944d3f7f9dee5008a3a24fd1a113c30d49d52948
rcolomina <rcolomina@localhost.localdomain>
2016-06-05 13:54:36 +0100

    Finish with BuildItemById. Now it is possible to create objects from XML file as input.

--- factory.cpp
+++ factory.cpp
@@ -14,19 +14,23 @@ FactoryGame::FactoryGame(string xmlGameSpecifications)
 }
 
 
-vector<string> FactoryGame::getIdListFromElement(pNode node,string nameAttribute){
-	  vector<string> output;
-
-
-	  
-	  return output;
-}
-
-vector<string> FactoryGame::getTextListFromElement(pNode parent){
+vector<string> FactoryGame::getNameListFromNode(pNode parent){
 
 	  vector<string> output;
 	  pNode child = 0;
-	  while(child = parent->IterateChildren(child))
+	  while((child = parent->IterateChildren(child)))
+	  {
+			 string value_text = child->Value();
+			 output.push_back(value_text);
+	  }
+	  return output;	  
+}
+
+vector<string> FactoryGame::getTextListFromNode(pNode parent){
+
+	  vector<string> output;
+	  pNode child = 0;
+	  while((child = parent->IterateChildren(child)))
 	  {
 			 pElem temp = child->ToElement();
 			 string value_text = temp->GetText();
@@ -49,8 +53,21 @@ pElem FactoryGame::getGameElement(string name){
 // Return the child element with a specific id 
 pElem FactoryGame::searchForElementId(pElem parent,string id){
 
+/*	  pNode child = parent->IterateChildren(child);
+	  while(child != NULL )
+	  {
+			 pElem temp = child->ToElement();
+			 string value_attribute = temp->Attribute("id");
+			 if( id.compare(value_attribute)==0 )
+			 {
+					return temp;
+			 }
+			 child = parent->IterateChildren(child);
+	  }
+*/
 	  pNode child = 0;
-	  while(child = parent->IterateChildren(child))
+//	  bool contin
+	  while((child = parent->IterateChildren(child)))
 	  {
 			 pElem temp = child->ToElement();
 			 string value_attribute = temp->Attribute("id");
@@ -59,42 +76,69 @@ pElem FactoryGame::searchForElementId(pElem parent,string id){
 					return temp;
 			 }
 	  }
+
+	  
 	  return NULL;
 }
 
 
 
 
-Objeto* FactoryGame::buildItemById(string itemId){
-	  
-	  Objeto* item=new Objeto();
+pItem FactoryGame::buildItemById(string itemId){
 
-	  pElem elem_Items=this->getGameElement(string("items"));	  
+	  pElem elem_Items=this->getGameElement(string("items"));	  	  
 	  pElem elem_Item=this->searchForElementId(elem_Items,itemId);
+
 
 	  // CREATING THE ITEM
 	  TiXmlHandle itemHandle(elem_Item);
 
 	  pElem elem_names=itemHandle.FirstChild("names").ToElement();
-	  vector<string> names=getTextListFromElement(elem_names);
+	  vector<string> names=getTextListFromNode(elem_names);
 	  vector<string>::iterator it;
 	  for(it=names.begin();it!=names.end();++it){
-			 cout<<*it<<endl;
+			 //	 cout<<*it<<endl;
 	  }
 			 
-
 	  pElem elem_descriptions=itemHandle.FirstChild("descriptions").ToElement();
-	  vector<string> descrip=getTextListFromElement(elem_descriptions);
+	  vector<string> descrip=getTextListFromNode(elem_descriptions);
 	  for(it=descrip.begin();it!=descrip.end();++it){
-			 cout<<*it<<endl;
+			 //	 cout<<*it<<endl;
 	  }
 
 	  pElem elem_properties=itemHandle.FirstChild("properties").ToElement();
-	  vector<string> prop=getTextListFromElement(elem_properties);
+	  vector<string> prop=getTextListFromNode(elem_properties);
 	  for(it=prop.begin();it!=prop.end();++it){
-			 cout<<*it<<endl;
+			 //	 cout<<*it<<endl;
 	  }
 
+	  vector<string> propN=getNameListFromNode(elem_properties);
+	  for(it=propN.begin();it!=propN.end();++it){
+			 // cout<<*it<<endl;
+	  }
+
+	  vector<string>::iterator it1;
+	  vector<string>::iterator it2;
+	  for(it1 = prop.begin(),it2 = propN.begin();
+			it1 < prop.end() && it2 < propN.end();
+			it1++,it2++)
+	  {
+			 
+
+	  }
+
+	  cout<<"names:"<<names[0]<<endl;
+	  cout<<"descrip:"<<descrip[0]<<endl;
+	  
+	  const Entidad& ent =  Entidad(1,names[0],descrip[0]);
+	  	  
+	  pItem item=new Item(ent,
+								 names,
+								 descrip,
+								 prop,
+								 propN);
+
+	  cout<<"Id:"<<item->Name();
 	  
 	  return item;
 }
--- factory.h
+++ factory.h
@@ -10,18 +10,24 @@ typedef TiXmlNode* pNode;
 typedef TiXmlElement* pElem;
 typedef TiXmlHandle* pHand;
 
+typedef Game* pGame;
+typedef Objeto* pItem;
+typedef Objeto Item;
+
+typedef Escenario* pScene;
+
 class FactoryGame{
 	public:
 	  FactoryGame(string xmlGameSpecifications);
 
-	  Game* buildGameById(string gameId);
-	  Escenario* buildScenarioById(string scenarioId);
-	  Objeto* buildItemById(string itemId);
+	  pGame buildGameById(string gameId);
+	  pScene buildScenarioById(string scenarioId);
+	  pItem buildItemById(string itemId);
 
 	private:
 	  
-	  vector<string> getIdListFromElement(pNode node,string nameAttribute);
-	  vector<string> getTextListFromElement(pNode node);
+	  vector<string> getNameListFromNode(pNode node);
+	  vector<string> getTextListFromNode(pNode node);
 
 	  pElem getGameElement(string name);
 	  pElem searchForElementId(pElem parent,string id);
