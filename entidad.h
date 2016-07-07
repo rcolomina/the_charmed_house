88be0aac2ac6de5c5c93ea1b997356d6893b990b
rcolomina <rcolomina@gmail.com>
2016-07-07 22:19:08 +0100

    Constructor deprecated

--- entidad.h
+++ entidad.h
@@ -14,21 +14,23 @@ class Entidad {
 			 name(name),
 	       description(description){}
 
-/*	  Entidad(const Entidad& other):id(other.id),
-											  name(other.name),
-											  description(other.description){}*/
-
+	  Entidad(const string sid,const string name,const string description):
+			 sid(sid),
+			 id(1),  //todo
+			 name(name),
+	       description(description){}
 
 	  Entidad(const Entidad &other):id(other.id),
 											  name(other.name),
 											  description(other.description){}
 
-	  
+	  const string sId(){return sid;}
 	  const int Id(){return id;}
 	  const string Name(){return name;}
 	  const string examine(){return description;}
 
 	protected:
+	  const string sid;
 	  const int id; 
 	  const string name;
 	  const string description;
