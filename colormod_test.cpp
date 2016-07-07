9445e6fd38e20794bcc63cfaaa288b7055260611
rcolomina <rcolomina@localhost.localdomain>
2016-07-07 19:29:47 +0100

    Class which encasulate color and bold font styles

---
+++ colormod_test.cpp
@@ -0,0 +1,13 @@
+#include "colormod.h" // namespace Color
+#include <iostream>
+using namespace std;
+using namespace Color;
+int main() {
+    cout << "This ->" << red << "word" << def << "<- is red." << endl;
+    cout << "This ->" << green << "word" << def << "<- is green." << endl;
+    cout << "This ->" << yellow << "word" << def << "<- is yellow." << endl;
+    cout << "This ->" << blue << "word" << def << "<- is blue." << endl;
+    cout << "This ->" << magenta << "word" << def << "<- is magenta." << endl;
+    cout << "This ->" << cyan << "word" << def << "<- is cyan." << endl;
+	 
+}
