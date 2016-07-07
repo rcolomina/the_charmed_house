d90a77a02437d81cd2b73a69c9c2ee071158c725
rcolomina <rcolomina@gmail.com>
2016-07-07 22:44:43 +0100

    changed names

--- main.cpp
+++ main.cpp
@@ -2,22 +2,24 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+#include <fstream>
+
 #include "manager.h"
 
 #include <tinyxml.h>
 
 // Thread used for main game
-void *hilo_principal(void *ptr);
+void *main_thread(void *ptr);
 
 // Thread used for time event
-void *hilo_eventos(void *ptr);
+void *events_thread(void *ptr);
 
 
 int main(int argc,char **n_args){
 
 	   pthread_t thread1,thread2;
 
- 	   TiXmlDocument doc("gamesInfo.xml");
+		/*	   TiXmlDocument doc("gamesInfo.xml");
 		bool loadOkay = doc.LoadFile();
 
 		if (!loadOkay)
@@ -36,7 +38,7 @@ int main(int argc,char **n_args){
 			  docHandle.FirstChild("game").FirstChild("scene").FirstChild("description").ToElement();
 
 		if(child2!=0)
-			  	cout<<child2->GetText()<<endl;
+			  	cout<<child2->GetText()<<endl;*/
 		
 		// ele1->SetAttribute( "id", "gameExample" );
       //	cout<<ele1->Attribute("id")<<endl;
@@ -54,18 +56,40 @@ int main(int argc,char **n_args){
 				
 	  Manager *manager=new Manager();
 
-	  pthread_create(&thread1, NULL, hilo_principal, (void*)manager);  //inteface de usuario
-	  pthread_create(&thread2, NULL, hilo_eventos, (void*)manager);    //eventos del juego
+	  pthread_create(&thread1, NULL, main_thread, (void*)manager);  //inteface de usuario
+	  pthread_create(&thread2, NULL, events_thread, (void*)manager);    //eventos del juego
 	  
-	  pthread_join( thread1, NULL); //espera hasta que acabe el hilo 1
+	  pthread_join( thread1, NULL); //espera haeeesta que acabe el hilo 1
 	  pthread_join( thread2, NULL); //espera hasta que acabe el hilo 2
 
-	  //delete manager;
+	  //Trying to save manager object.
+/*	  std::ofstream file("./saved.bin");
+
+//	  ofs << manager; // store the object to file
+//	  ofs.close();
+//
+	  int beginning,end;
+
+	  beginning = file.tellg();
+	  file.seekg(0, ios::end);
+	  end = file.tellg();
+
+	  ifstream::pos_type size = (end - beginning);
+	  
+
+	  file.seekg(0,ios::beg);
+	  file.read((char*)&memblock,size);
+	  file.close();
+*/
+
+	  
+	  
+	  delete manager;
 
 	  return 0;
 }
 
-void *hilo_principal( void *ptr )
+void *main_thread( void *ptr )
 {
 	  Manager *manager;
 	  manager = (Manager *) ptr;
@@ -73,7 +97,7 @@ void *hilo_principal( void *ptr )
 	  return 0;
 }
 
-void *hilo_eventos( void *ptr )
+void *events_thread( void *ptr )
 {
 	  Manager *manager;
 	  manager = (Manager *) ptr;
