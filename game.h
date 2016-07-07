9c714097f412378b82cd584c6a5f3c20e77830cf
rcolomina <rcolomina@gmail.com>
2016-07-07 22:44:04 +0100

    Chaged names

--- game.h
+++ game.h
@@ -1,39 +1,54 @@
-#ifndef _GAME_
-#define _GAME_
+#ifndef _game_
+#define _game_
 
 #include <map>
+#include <iostream>
 
-#include "objeto.h"
+
+#include "operaciones.h"
+//#include "event.h"
 
 using namespace std;
 
-class Escenario;
+//class Escenario;
+//class Objeto;
 
+//typedef Escenario* pScene;
+//typedef Objeto* pItem;
 // This class is an interface to storage
 // for scenearios and items.
 // It saves the state of the world game
+
+//class Escenario;
+
+
+class Game;
+typedef Game* pGame;
+
 class Game : public Entidad{
 	public:
 	  Game();
+	  Game(string idGame,string xmlSpecs){}
 
-	  Escenario *get_scenario(string key);
+	  pScene get_scenario(string key);
 	  void set_scenario(Escenario *scenario);
 
-	  Objeto *get_item(string key);
+	  pItem get_item(string key);
 	  void set_item(Objeto *item);
 
-	  Escenario *get_current_scene();
+	  pScene get_current_scene();
 	  void set_current_scene();
 
 	  void save_game(string game_name);
 	  void load_game(string game_name);
+
 	private:
-	  // Save the state of the game world
-	  map<string,Escenario*> world;
-	  map<string,Objeto> items;
+	  // Storing the state of the game world
+	  map<string,pScene> world;
+	  map<string,pItem> items;
 
 	  // References to the current scene
-	  Escenario *current_scene; 
+	  pScene current_scene; 
 	  //Escenario current_scene1; 
 
 };
