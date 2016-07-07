5d2bbe5c1f3a5abe36c277a19d9e8211da512b02
rcolomina <rcolomina@gmail.com>
2016-07-07 22:51:47 +0100

    Removed all constants values that are reading from XML

--- parametros.h
+++ parametros.h
@@ -3,12 +3,46 @@
     
 #include <string>
 #include <iostream>
+#include <map>
+#include <vector>
 
-#define NUMBER_COMMANDS 13
+#define NUMBER_COMMANDS 14
 
 using namespace std;
   
 namespace parametros{
+
+/*
+
+	  //TODO: Create list from XML game
+	  const map<string,pair<string,TYPE_VAR> >  listDefaultProperties =
+	  {{string("used"),paFalse},
+		{string("fix"),paFalse},
+		{string("visibility"),paTrue},
+		{string("tirable"),paFalse}};
+
+
+*/
+
+	  enum TYPE_VAR {BOOLEAN,INTEGER,FLOAT,DOUBLE,STRING};
+	  
+	  const string sFalse = string("false");
+	  const string sTrue  = string("true");
+	  
+	  const pair<string,TYPE_VAR> paFalse = {sFalse,BOOLEAN};
+	  const pair<string,TYPE_VAR> paTrue  = {sTrue,BOOLEAN};
+	  
+	  const map<string,pair<string,TYPE_VAR> >  listDefaultProperties =
+	  {{string("used"),paFalse},
+		{string("fix"),paFalse},
+		{string("visibility"),paTrue},
+		{string("tirable"),paFalse}};
+
+	  const vector<string> commands = {
+			 "exit","examinar","ver","inventario","salidas","norte(n)",
+			 "sur(s)","este(e)","oeste(o)","tiempo"};
+
+	  
 	  const string c1="exit";
 	  const string c2="examinar";
 	  const string c3="ver";
@@ -38,6 +72,7 @@ namespace parametros{
 	  const string s_dejar="dejar";
 	  const string s_alcanzar="alcanzar";
 	  const string s_space=" ";
+	  const string s_colocar="colocar";
 	  
 	  const string oeste="oeste";
 	  const string este="este";
@@ -52,63 +87,46 @@ namespace parametros{
 	  const string tirar="tirar";
 	  const string empujar="empujar";
 	  const string pulsar="pulsar";
-
-
 	  
 	  const string comandos[NUMBER_COMMANDS]={s_exit,
-										  s_salir,
-										  s_examinar,
-										  s_ver,
-										  s_inventario, 
-										  s_salidas,
-										  s_norte,
-										  s_sur,
-										  s_este,
-										  s_oeste,
-										  s_tiempo,
-										  s_tirar,
-										  s_alcanzar};
+															s_salir,
+															s_examinar,
+															s_ver,
+															s_inventario, 
+															s_salidas,
+															s_norte,
+															s_sur,
+															s_este,
+															s_oeste,
+															s_tiempo,
+															s_tirar,
+															s_alcanzar,
+															s_colocar};
 
 
-     //DESCRIPCIONES ESCENARIOS: TODO ¿Cómo organizar las descripciones y observaciones de un escenario?
-
-	  const int id_escena1=1;
+	  enum cardinal{
+			 north, northwest,west,southwest, south, southeast, east,northeast};
+			 
 
 	  const string tituloPreludio="PRELUDIO:";
+
+	  const string nombre1="La Entrada a la Casa";
+	  const string nombre2="El monumento de piedras";
+	  const string nombre3="Cementerio"; 
+	  
+     //DESCRIPCIONES ESCENARIOS: TODO ¿Cómo organizar las descripciones y observaciones de un escenario?
+
+	  // const int id_escena1=1;
+
+	  // const string tituloPreludio="PRELUDIO:";
 
  	  const string textoPreludio1="En un pueblo lejos de la ciudad, había una casa ruinosa y antigua desde que tengo ojos para ver. Como aventurero que soy, he tenido en mente explorarla en todo momento, pero nunca me he atrevido. Existen carteles prohibiendo el paso en sus alrededores. Quien quiera que sea el dueño, desea que los intrusos permanezcan lejos.";
 	  const string textoPreludio2="Durante noches había estado soñando con aquella casa en ruinas, imaginando que podría haber en su inferior. Una noche, en uno de mis sueños, vislumbré una figura humana irreconocible. Lo único que pude descifrar en aquel enigmático sueño, es la llamada de aquella entidad. Susurraba, -Ayuuudame!!, ayuudame!!-.  \n Desde aquel momento, no pude esperar más. Debía entrar en aquella casa abandonada,  a pesar de que las advertencias en los alrededores.";
 
-	 const string textoComienzo="La aventura comienza…" ; 
+	  const string textoComienzo="La aventura comienza…" ; 
 
-	  const int id1=1;
-	  const string nombre1="La Entrada a la Casa";
-	  const string escena1=nombre1;
-	  const string descripcion1="Una casa antigua construída en piedra y madera se presenta ante tí. \nA duras penas se sostiene sobre sus\n cimientos.\nHay carteles de cortesia advirtiendo a los intrusos. \n¡ATENCION! NO PASAR, PELIGRO, FUERA!, A LA MIERDA, ¿NO TE HAS ENTERADO?.";
-	  const string observacion1="Puedes ver una rama de árbol indicando ladrillo en la pared";
-
-	  const int id2=2;
-	  const string nombre2="El monumento de piedras";
-	  const string escena2=nombre2;
-	  const string descripcion2="Piedras amontonadas en tres grupos parecían representar algún tipo de monumento en otro tiempo.";
-	  const string observacion2="Las piedras están amontonadas en 3 grupos, cada uno con un número determinado. Es una combinación de 3 números.";
-
-	  const int id3=3;
-	  const string nombre3="Cementerio";
-	  const string escena3=nombre3;
-	  const string descripcion3="Cerca de la casa hay un pequeños cementerio. En una de las lápidas puedes leer unas extrañas inscripciones grabadas en forma de árbol.";
-	  const string observacion3="Mirando atentamente sobre una de las lápida, descubres una inscripción: I-D-I-I-D-D-I-D-I-D-D-D. ¿Podrían ser paso de baile?";
-
-	  const int id4=4;
-	  const string nombre4="Arco de arboles";
-	  const string escena4=nombre4;	  
-	  const string descripcion4="Un arco de piedra se alza en tu camino. Puedes ver que un objeto metálico en su parte mas alta. No crees que puedas alcanzarlo con las manos desnudas."; 
-	  const string observacion4="Puedes ver que el objeto es una palanca para tirar de cualquier cosa";
-
-	  const string descripcion5="";
-	  //OBJETOS DEL MUNDO
-	  
-
+	
+	  const string postMessageActivationToEventOpenConnection1 = string("Unas rocas se levantan de las profundidades del lago, formando un pasadizo que te permite acceder al centro de la isla. Una nueva salida se abre hacia el oeste.");
 
 }
 
