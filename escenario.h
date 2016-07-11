8c031fa2dda4a72ae208cddf5e2b26eefb4c39ff
rcolomina <rcolomina@gmail.com>
2016-07-11 21:30:52 +0100

    Cleaning comments

--- escenario.h
+++ escenario.h
@@ -12,70 +12,52 @@ using namespace parametros;
 using namespace std;
 
 class Escenario;
-
 typedef Escenario* pScene;
 typedef Escenario** ppScene;
 
 extern void connect(Escenario* scenA,cardinal carA,Escenario* scenB,cardinal carB);
 
-
 //Clase que describe un escenario: Salidas, Objetos, Descripción, Imagen
 class Escenario : public Entidad {
-	public:
-	  //constructores
-	  Escenario(){}
-	  Escenario(int id,string nombre,string descripcion,string observacion);
-	  Escenario(const Entidad &ent,const string &observacion,const string &path);
+   public:
+     //constructores
+     Escenario(){}
+     Escenario(int id,string nombre,string descripcion,string observacion);
+     Escenario(const Entidad &ent,const string &observacion,const string &path);
+     
+     //////getters//////
+     string get_description();
+     string salidas();     
+     string get_observacion(){return observacion;}
+     map<string,pScene> get_salidas(){return escenarioSalidas;} //devuelve lista de escenarios salidas
+     string salidasDisponibles();	 
+     pScene get_salida(string coordenada){return escenarioSalidas[coordenada];} // return null if does not exist
+     pScene conexion(cardinal car);
+     bool get_estado_salida(string coordenada);
+     map<string,pItem> get_objetos(){return objetos;} // what objects are availables
+     string get_objetos_disponibles();	  
+     pItem get_objeto(string nombre){return objetos[nombre];}
+     bool get_existe_objeto_escenario(string nombre);
+     string get_path_image(){return pathFileImageText;}
 	  
-	  //////getters//////
-	  string get_observacion(){return observacion;}
-	  map<string,pScene> get_salidas(){return escenarioSalidas;} //devuelve lista de escenarios salidas
-	  string salidasDisponibles();	 
-	  pScene get_salida(string coordenada){return escenarioSalidas[coordenada];} //devuelve nulo si no existe
-	  pScene conexion(cardinal car);
-	  bool get_estado_salida(string coordenada);
-	  map<string,pItem> get_objetos(){return objetos;} //que objetos hay disponible
-	  string get_objetos_disponibles();
-	  
-	  pItem get_objeto(string nombre){return objetos[nombre];}
-	  bool get_existe_objeto_escenario(string nombre);
+     //////setters/////
+     void set_salidas(map<string,pScene> salidas); //se invocará en la construcción del mundo	  	  
+     void set_salida(pScene escenario,string coordenada,bool estado);
+     void set_estado_salida(string coordenada,bool new_estado);
+     void set_objeto(pItem obj);
+     void eliminar(pItem obj);
 
-	  string get_path_image(){return pathFileImageText;}
-	  
-	  //////setters/////
-	  void set_salidas(map<string,pScene> salidas); //se invocará en la construcción del mundo	  	  
-	  void set_salida(pScene escenario,string coordenada,bool estado);
-	  void set_estado_salida(string coordenada,bool new_estado);
-	  void set_objeto(pItem obj);
-	  void eliminar(pItem obj);
-
-	  // procedures
-	  void pintar(); //permite pintar el escenario
-
-//	  void popEvent();
-//	  void pushEvent(IEvent *event);
-	private:	
-	  string observacion;
-	  
-	  map<string,pScene> escenarioSalidas;       // guarda par (cardinal, pScene)
-	  map<string,bool>   estadoSalidas; // salida abierta o cerrada
-	  map<string,pItem>  objetos;       // nombre y referenca a objeto
-	  
-	  const string pathFileImageText;
-	  
-	  map<cardinal,pScene> conexions;
-	  friend void connect(Escenario* scenA,cardinal carA,Escenario* scenB,cardinal carB);
-
+     // procedures
+     void pintar(); // allow to draw a scene
+   private:	
+     string observacion;	  
+     map<string,pScene> escenarioSalidas;       // guarda par (cardinal, pScene)
+     map<string,bool>   estadoSalidas; // salida abierta o cerrada
+     map<string,pItem>  objetos;       // nombre y referenca a objeto	 
+     const string pathFileImageText;	  
+     map<cardinal,pScene> conexions;
+     friend void connect(Escenario* scenA,cardinal carA,Escenario* scenB,cardinal carB);
 };
-
-
-/*
-class ScenePuzzle : public Escenario{
-	public:
-	  ScenePuzzle(){}
-
-};*/
-
 
 #endif
 
